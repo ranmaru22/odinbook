39b85e07129b1619acc0e32fb014dcc1207a18da
Alex Sun <alexsun82@icloud.com>
2020-06-12 18:44:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ODINBOOK
+
+A Facebook clone made with Node.js/Express and TypeScript.
+
+Final capstone project for [The Odin Project](https://www.theodinproject.com).
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import App from "./app";
+import app from "./app";
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
