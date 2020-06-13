663f8512d605c042520455e6d3ee0d3b5080c136
Alex Sun <alexsun82@icloud.com>
2020-06-13 12:10:19 +0200

    Add null check to port variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import App from "./app";
 import app from "./app";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
