b12d63adad6cab229883e6c3ed6ff44bac3bd2a8
Alex Sun <alexsun82@icloud.com>
2020-06-14 00:45:20 +0200

    Refactor listen function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import app from "./app";
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(app.get("port"), () => console.log(`Server running on port ${app.get("port")}`));
