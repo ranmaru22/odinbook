3a70515dabc6b9766200475c86a18614ac7ec91d
Alex Sun <alexsun82@icloud.com>
2020-06-14 00:46:27 +0200

    Update model + interface

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,19 +1,35 @@
 import mongoose from "mongoose";
+import { IPost } from "./post";
 
 export interface IUser extends mongoose.Document {
     name: string;
     email: string;
     password: string;
     joindate: Date;
-    friends: IUser[] | string[];
+    friends?: IUser[] | string[];
+    friendRequests?: IUser[] | string[];
+    likedPosts?: IPost[] | string[];
+    url: string;
+    posts: IPost[] | string[];
 }
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true, min: 3 },
-    email: { type: String, required: true, unique: true, min: 3 },
+const userSchema = new mongoose.Schema<IUser>({
+    name: { type: String, required: true, minlength: 3 },
+    email: { type: String, required: true, unique: true, minlength: 3 },
     password: { type: String, required: true },
     joindate: { type: Date, default: Date.now },
-    friends: [{ type: mongoose.Types.ObjectId, ref: "User" }]
+    friends: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+    friendRequests: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+    likedPosts: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
+});
+
+userSchema.virtual("url")
+    .get((function (this: mongoose.Document): string { return `/user/${this._id}` }));
+
+userSchema.virtual("posts", {
+    ref: "Post",
+    localField: "_id",
+    foreignField: "author"
 });
 
 const userModel = mongoose.model<IUser>("User", userSchema);
