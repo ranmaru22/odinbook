957dfeefa3990c7ab4cfde891240058f0ea0d889
Alex Sun <alexsun82@icloud.com>
2020-06-15 01:11:01 +0200

    Add confirmation functions for posts and profiles

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+import Post, { IPost } from "../models/post";
+import User, { IUser } from "../models/user";
+import Profile, { IProfile } from "../models/profile";
+import mongoose from "mongoose";
 
 namespace Auth {
     export function protectRoute(req: Request, res: Response, next: NextFunction): void {
@@ -6,6 +10,32 @@ namespace Auth {
             next();
         } else {
             res.redirect("/");
+        }
+    }
+
+    export async function confirmOwnerPost(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const post = await Post.findById(req.params.id).exec();
+            if ((post?.author as mongoose.Document).equals(req.user as mongoose.Document)) {
+                next()
+            } else {
+                res.redirect("back");
+            }
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    export async function confirmOwnerProfile(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const profile = await Profile.findById(req.params.id).exec();
+            if ((profile?.owner as mongoose.Document).equals(req.user as mongoose.Document)) {
+                next()
+            } else {
+                res.redirect("back");
+            }
+        } catch (err) {
+            return next(err);
         }
     }
 }
