644f31317d75ef408ebe931d08a470769a0039de
Alex Sun <alexsun82@icloud.com>
2020-06-14 16:22:15 +0200

    Add posts controller

---
+++ src/controllers/postsController.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from "express";
+import * as validator from "express-validator";
+
+import User, { IUser } from "../models/user";
+import Profile, { IProfile } from "../models/profile";
+import Post, { IPost } from "../models/post";
+
+/*
+ * GET / - Homepage
+*/
+
+export default class PostsController {
+    static postValidationChain: validator.ValidationChain[] = [
+        validator.body("post").trim().isLength({ min: 3 }).withMessage("Text can't be less than 3 characters.")
+    ];
+
+    static index(req: Request, res: Response): Response {
+        return res.send("index");
+    }
+
+    static async newPost(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const validationErrors = validator.validationResult(req);
+            if (!validationErrors.isEmpty()) {
+                return res.redirect(400, "back");
+            } else {
+                const author = await User.findById(req.user).exec();
+                const post = new Post({
+                    text: req.body.post,
+                    author: author
+                });
+                if (req.body.parentpost) {
+                    post.parent = req.body.parentpost;
+                }
+                const savedPost = await post.save();
+                if (req.body.profileId) {
+                    const success = await Profile.updateOne(
+                        { _id: req.body.profileId, owner: req.user },
+                        { $push: { posts: savedPost } }
+                    );
+                    if (success.nModified === 0) {
+                        await savedPost.remove();
+                    }
+                }
+                return res.redirect("back");
+            }
+        } catch (err) {
+            return next(err);
+        }
+    }
+}
+
