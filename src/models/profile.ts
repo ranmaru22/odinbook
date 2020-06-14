d2b2d1e5de56fabc2e88e454342731230d422e32
Alex Sun <alexsun82@icloud.com>
2020-06-14 10:31:08 +0200

    Update profile model + interface

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -4,11 +4,16 @@ import { IPost } from "./post";
 
 export interface IProfile extends mongoose.Document {
     owner: IUser | string;
-    picture: string;
+    picture?: string;
+    status?: string;
+    posts?: IPost[] | string[];
 }
 
 const profileSchema = new mongoose.Schema({
-
+    owner: { type: mongoose.Types.ObjectId, ref: "User", required: true, unique: true },
+    picture: { type: String },
+    status: { type: String },
+    posts: [{ type: mongoose.Types.ObjectId, ref: "Post" }]
 });
 
 const profileModel = mongoose.model<IProfile>("Profile", profileSchema);
