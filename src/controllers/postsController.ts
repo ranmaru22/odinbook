020621c40267cefe48a71bf188733415f23aba7b
Alex Sun <alexsun82@icloud.com>
2020-06-17 01:12:29 +0200

    Remove debug messages

--- src/controllers/postsController.ts
+++ src/controllers/postsController.ts
@@ -53,13 +53,9 @@ export default class PostsController {
             const post = await Post.findOne({ _id: req.params.id }).exec();
             console.log(post?.likedBy);
             if (post?.likedBy?.indexOf((req.user as IUser)._id) === -1) {
-                console.log("LIKE++");
                 await Post.updateOne(post!, { $push: { likedBy: req.user as IUser } });
-                console.log(post?.likedBy);
             } else {
-                console.log("LIKE--");
                 await Post.updateOne(post!, { $pull: { likedBy: (req.user as IUser)._id } });
-                console.log(post?.likedBy);
             }
             await post?.save();
             res.redirect("back");
