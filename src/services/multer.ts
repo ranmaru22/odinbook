3de5ccd60cd766cd54cf1a68f8059d8f1e197d7c
Alex Sun <alexsun82@icloud.com>
2020-06-19 16:36:32 +0200

    Add method to get a Data URI from an uploaded file

--- src/services/multer.ts
+++ src/services/multer.ts
@@ -1,8 +1,12 @@
 import multer from "multer";
+import DatauriParser from "datauri/parser";
+import path from "path";
+
 
 namespace Multer {
     export const storage = multer.memoryStorage();
     export const uploads = multer({ storage }).single("image");
+    export const dataUri = ( req: any ) => new DatauriParser().format(path.extname(req.file.originalname), req.file.buffer);
 }
 
 export default Multer;
